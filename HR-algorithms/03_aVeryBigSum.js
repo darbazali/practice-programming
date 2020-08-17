61f17e91195dc39585909fc16bf022c0fa2d624b
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-17 16:36:29 +0300

    A very big sum

---
+++ HR-algorithms/03_aVeryBigSum.js
@@ -0,0 +1,45 @@
+/* 
+In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+aVeryBigSum has the following parameter(s):
+
+    int ar[n]: an array of integers .
+
+Return
+
+    int: the sum of all array elements
+
+Input Format
+
+The first line of the input consists of an integer
+.
+The next line contains
+
+space-separated integers contained in the array.
+
+Output Format
+
+Return the integer sum of the elements in the array.
+
+Constraints
+
+Sample Input
+
+5
+1000000001 1000000002 1000000003 1000000004 1000000005
+
+Output
+
+5000000015
+
+*/
+
+const aVeryBigSum = arr => arr.reduce((item, index) => item + index);
+
+const bigSum = aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]);
+
+console.log(bigSum);    // returns 5000000015
