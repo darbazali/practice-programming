f6e666306cec689de988e5f9fba5efe6d55de5ba
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-20 16:51:24 +0300

    refactor miniMaxSum

--- HR-algorithms/08_miniMaxSum.js
+++ HR-algorithms/08_miniMaxSum.js
@@ -3,25 +3,26 @@ console.clear();
 
 const miniMaxSum = arr => {
 
-    // find min, max from srouce array
-    let min = Math.min(...arr);
-    let max = Math.max(...arr);
+    /* 
+        Helper functions
+        sum: reutrns one single value from suming an array
+        removeItem: removes an elment from array based on a given index
+    */
+    const sum = arr => arr.reduce((prev, curr) => prev + curr);
+    const removeItem = (arr, index) => arr.splice(arr.indexOf(index), 1);
+
+    // find min, max values of the source array
+    let minVal = Math.min(...arr);
+    let maxVal = Math.max(...arr);
 
     // create new array for 2 cases
     const minArr = [...arr]
     const maxArr = [...arr]
 
-    // remove min value from maxArr
-    const removeMin = maxArr.splice(arr.indexOf(min), 1);
-    // remove max value from minArr
-    const removeMax = minArr.splice(arr.indexOf(max), 1);
+    removeItem(maxArr, minVal);     // remove min value from maxArr
+    removeItem(minArr, maxVal);     // remove max value from minArr
 
-    // sum minArr
-   const mini = minArr.reduce((prev, curr) => prev + curr)
-   // sum maxArr
-   const maxi = maxArr.reduce((prev, curr) => prev + curr)
-
-    console.log(mini, maxi)
+    console.log(sum(minArr), sum(maxArr))
 }
 
 
