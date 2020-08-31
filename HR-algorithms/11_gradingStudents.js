0142ab24b1ca0c217e48373455d41824ce41dd53
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-31 14:07:48 +0300

    find next multiple of number

--- HR-algorithms/11_gradingStudents.js
+++ HR-algorithms/11_gradingStudents.js
@@ -1,14 +1,30 @@
 
-
+const nextMultipleOfFive = number => {
+    for ( let i = number; i <= 100, i++;) {
+        if ( i % 5 == 0 || i % 1 == 1) {
+            return i;
+        }
+    }
+}
 const gradingStudents = grades => {
     
-    return grades.map( grade => {
-        if (grade < 38 ) grade = grade;
-        else if ( grade > 38 && grade < 40 ) grade = 40
-        // else if ( grade > 40 && grade)
-    })
+    let result = [];
 
+    for ( let item in grades ) {
+        let grade = grades[item];
+
+        // if grade - next x5 < 3 => grade - next x5
+
+
+        if ( grade < 38 ) {
+            result.push(grade);
+        }
+    }
+    return result;
 }
 
 const arr = [73, 67, 38, 33];
 console.log(gradingStudents(arr));  // should return 75 67 40 33
+
+console.log(nextMultipleOfFive(73))
+console.log(nextMultipleOfFive(38))
