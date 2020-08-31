c16287e53b1b37234d65d7e7471c0f3186fee9b3
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-31 14:45:17 +0300

    refactory

--- HR-algorithms/11_gradingStudents.js
+++ HR-algorithms/11_gradingStudents.js
@@ -1,46 +1,47 @@
 
 
 // given a number, the func should return next multiple of 5 of this number
-const nextMultipleOfFive = number => {
-    for ( let i = number; i <= 100, i++;) {
-        if ( i % 5 == 0 || i % 1 == 1) {
-            return i;
-        }
-    }
-}
+
 
 
 const gradingStudents = grades => {
     
-//     let result = [];
+    // helper function
+    // given a number, should return a number that is
+    // the next muliple of 5
+    const nextMultipleOfFive = number => {
 
-//     grades.forEach(item => {
-//         // if item < 38, return item
-//         if ( item < 38 ) {
-//             result.push(item);
-//         }
-//     });
+        // create a loop between number and 100, <= 0 number <= 100
+        for ( let i = number; i <= 100, i++;) {
+            // if number is divisible by 5 or one, return this number
+            if ( i % 5 == 0 || i % 1 == 1) {
+                return i;
+            }
+        }
+    }
 
-//     return result;
-
+    // map throught grades, array
     return grades.map( item => {
-        if ( item >= 38 && (nextMultipleOfFive(item) - item < 3)) {
-            return item = nextMultipleOfFive(item)
+        // rules
+        // if number >= 38
+            // if next X5 - grade < 3, then the grade should be next X5
+            // if next X5 - grade >= 3, then return the grade
+        // if number < 30, no changes will be occured
+        if ( item >= 38 ) {
+            if ( nextMultipleOfFive(item) - item < 3) {
+                return item = nextMultipleOfFive(item)
+            } else {
+                return item
+            }
         }
 
-        if ( item >= 38 && ( nextMultipleOfFive(item) - item >= 3 )) {
-            return item = item;
-        }
         else {
-            return item
+            return item;
         }
     })
 }
 
-const arr = [73, 67, 38, 33];
-console.clear();
-console.log(gradingStudents(arr));  // should return 75 67 40 33
 
-// console.log(nextMultipleOfFive(73))
-// console.log(nextMultipleOfFive(67))
-// console.log(nextMultipleOfFive(38))
+
+const arr = [73, 67, 38, 33];
+console.log(gradingStudents(arr));  // should return 75 67 40 33
