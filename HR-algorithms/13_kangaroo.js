db9e8f70ad3250545dafaf2375484234839234b2
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-01 16:00:45 +0300

    refactor #13

--- HR-algorithms/13_kangaroo.js
+++ HR-algorithms/13_kangaroo.js
@@ -1,17 +1,31 @@
 
 
+console.clear();
+
 
 const kangaroo = (x1, v1, x2, v2) => {
 
-    if ( x1 >= x2 && v1 >= v2 ) {
-        return 'NO'
-    } else if ( x2 >= x1 && v2 >= v1 ) {
-        return 'NO'
-    } else {
-        return 'YES'
-    }
-}
+    // if ( (x1 > x2 && v1 && v2) || ( x2 > x1 && v2 > v1) ) return 'NO'
 
+    const end = 10000;
+    let lx1 = 0
+    let lx2 = 0
+    let result = 'NO';
+
+    for( let i = x1, j = x2; i <= end, j <= end; i = i + v1 , j = j + v2) {
+        lx1 = i;
+        lx2 = j;
+
+        if ( lx1 === lx2 ) {
+            result = 'YES'
+            break;
+        }
+    }
+ 
+    return result;
+
+}
 
 console.log(kangaroo(0, 2, 5, 3))
 console.log(kangaroo(0, 3, 4, 2))
+console.log(kangaroo(4523, 8092, 9419, 8076))   // should YES
