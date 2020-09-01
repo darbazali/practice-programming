2d9da4fae4948293837da84ed8cc316df7ec3525
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-01 16:13:26 +0300

    pass all tests #13

--- HR-algorithms/13_kangaroo.js
+++ HR-algorithms/13_kangaroo.js
@@ -7,23 +7,15 @@ const kangaroo = (x1, v1, x2, v2) => {
 
     // if ( (x1 > x2 && v1 && v2) || ( x2 > x1 && v2 > v1) ) return 'NO'
 
-    const end = 10000;
-    let lx1 = 0
-    let lx2 = 0
     let result = 'NO';
 
-    for( let i = x1, j = x2; i <= end, j <= end; i = i + v1 , j = j + v2) {
-        lx1 = i;
-        lx2 = j;
-
-        if ( lx1 === lx2 ) {
-            result = 'YES'
-            break;
+    for (let i = 0; i < 10000 && result == 'NO'; i++) {
+        if (x1 + v1 * i == x2 + v2 * i) {
+            result = 'YES';
         }
     }
- 
-    return result;
 
+    return result;
 }
 
 console.log(kangaroo(0, 2, 5, 3))
