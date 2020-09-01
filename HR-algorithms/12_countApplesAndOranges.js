9a897f6b1e6c8f43c3bf82a925e91d3bab7b3767
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-01 14:05:28 +0300

    refactor #12

--- HR-algorithms/12_countApplesAndOranges.js
+++ HR-algorithms/12_countApplesAndOranges.js
@@ -1,35 +1,21 @@
 
 
-/* 
-
-def countApplesAndOranges(s, t, a, b, apples, oranges):
-    closeapples, closeoranges = 0, 0
-    for d in apples:
-        if a+d>=s and a+d<=t:
-            closeapples+=1
-    for d in oranges:
-        if b+d<=t and b+d>=s:
-            closeoranges+=1
-    print(closeapples)
-    print(closeoranges)
-
-*/
-
 const countApplesAndOranges = ( s, t, a, b, apples, oranges) => {
-    let closeapples = 0, closeoranges = 0;
+    let closeApples = 0, closeOranges = 0;
     
     apples.forEach(d => {
         if ( a + d >= s && a + d <= t) {
-            closeapples += 1
+            closeApples += 1
         }
     });
 
     // oranges
     oranges.forEach(d => {
         if ( b + d  <= t && b + d >= s) {
-            closeoranges += 1;
+            closeOranges += 1;
         }
     });
 
-    console.log(closeapples, closeoranges)
+    console.log(closeApples)
+    console.log(closeOranges);
 }
