3882c93c0fe073700fc120d7d2782df0f7a33a74
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-13 22:34:11 +0300

    async calculatin

--- general/asycAwait.js
+++ general/asycAwait.js
@@ -5,6 +5,14 @@ console.clear();
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 const getUsers = async () => await fetch(url).then(res => res.json());
 
-getUsers()
-    .then(data => console.log(data))
-    .catch( err => console.log(err))
+// getUsers()
+//     .then(data => console.log(data))
+//     .catch( err => console.log(err))
+
+
+const calculateData = async (x, y) => {
+    return await x + y
+}
+
+calculateData(10, 10)
+    .then( res => console.log(res) )
